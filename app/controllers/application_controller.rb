55b921514cb0e26cd57eeba3aa7a6500278a563c
Sanju Basnet <sanjubasnet@bajratechnologies.com>
2022-02-10 16:04:08 +0545

    feat: image upload as profile pic complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:user_name,:role, :surname, :phone_no, :email, :password)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name,:role, :surname, :phone_no, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:user_name,:role, :surname, :phone_no,:profile_pic, :email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name,:role, :surname, :phone_no,:profile_pic, :email, :password, :current_password)}
   end
 end
