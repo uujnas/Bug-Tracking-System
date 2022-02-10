a9ffbb9782db865b2dbe14e7861599dc44f72a20
Sanju Basnet <sanjubasnet@bajratechnologies.com>
2022-02-10 16:42:50 +0545

    feat: image upload validation done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_one_attached :profile_pic
+  validates :profile_pic, presence: true, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..5.megabytes }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
