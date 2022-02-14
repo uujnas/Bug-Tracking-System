afcfa078f9163e83b505aa0cf2b9a95740fcb49b
Sanju Basnet <sanjubasnet@bajratechnologies.com>
2022-02-14 19:34:18 +0545

    fix: layout and rendering

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,4 @@
-class ProjectsController < ApplicationController
+class ProjectsController < RoleDashboardsController
   before_action :authenticate_user!
   before_action :set_project, only: %i[show edit update destroy]
 
--- app/controllers/role_dashboards_controller.rb
+++ app/controllers/role_dashboards_controller.rb
@@ -1,6 +1,7 @@
 class RoleDashboardsController < ApplicationController
     before_action :authenticate_user!
-  
+    layout 'dashboard'  
+
     def index
     end
 end
