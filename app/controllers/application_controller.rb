e5639957cf57d08f18bd7914e323d74fb93c59cb
Sanju Basnet <sanjubasnet@bajratechnologies.com>
2022-02-16 13:15:24 +0545

    fix: cancan::accessdenied error solved instead flash alert msg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+rescue_from CanCan::AccessDenied do |exception|
+  redirect_to role_dashboards_path, :alert => exception.message
+end
+
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
