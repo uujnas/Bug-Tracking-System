f418b6b2416ecd5a80939bdb7dd9f683dd2ca2a7
Sanju Basnet <sanjubasnet@bajratechnologies.com>
2022-02-16 17:25:05 +0545

    fix: foreign key violation error fix while deleting dependent data

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,6 +1,8 @@
 class Bug < ApplicationRecord
   belongs_to :user
   belongs_to :project
+  has_many :assign_bugs, dependent: :destroy
+
   validates :title, presence: true, uniqueness: true
 
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
   belongs_to :user
-  has_many :bug
+  has_many :bug, dependent: :destroy
   validates :title, presence: true, uniqueness: true
 end
